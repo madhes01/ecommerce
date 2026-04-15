39e29a578396d48a10269ab4383f75699d2436b3
Madheswaran <madhankrishnan98@gmail.com>
2026-04-15 16:56:20 +0530

    commit

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,8 +1,11 @@
 import { type SchemaTypeDefinition } from "sanity";
 import { customerType } from "./customerType";
+import { categoryType } from "./categoryType";
+import { productType } from "./productType";
+import { orderType } from "./orderType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
     types: [
-        customerType,
+        customerType, categoryType, productType, orderType
     ],
 }
